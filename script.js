12db7f213c2de785f1d93e2dee221e72bdc63a51
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2024-11-16 10:35:25 +0530

    Home section has been completely remodified

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-// Toggle Dark Mode
+// <=========== Change Dark Mode ===========>
+
+document.addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.key === "d") {
+        event.preventDefault();
+        darkMode();
+    }
+});
+
 function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-    
+
     var icon = document.querySelector('.navbar-container .content button i');
     if (element.classList.contains("dark-mode")) {
         icon.classList.replace("ri-moon-clear-line", "ri-sun-line");
@@ -12,7 +20,14 @@ function darkMode() {
 }
 
 
-// Active navbar
+
+
+
+
+
+
+// <=========== Active navbar ===========>
+
 function setActiveLink() {
     const sections = document.querySelectorAll('.home-container, .about-container, .products-container, .faq-container, .contact-container');
     const navLinks = document.querySelectorAll('.navbar-container .content a');
@@ -50,7 +65,57 @@ window.addEventListener('scroll', setActiveLink);
 setActiveLink();
 
 
-// Image Filter
+
+
+
+
+
+
+// <=========== Home Image Sliding ===========>
+
+const slides = document.querySelectorAll('.slide-item');
+const dots = document.querySelectorAll('.dots');
+let currentIndex = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = 'none';
+        dots[i].classList.remove('active');
+    });
+
+    slides[index].style.display = 'flex';
+    dots[index].classList.add('active');
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex === 0) ? slides.length - 1 : currentIndex - 1;
+    showSlide(currentIndex);
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex === slides.length - 1) ? 0 : currentIndex + 1;
+    showSlide(currentIndex);
+}
+
+function goToSlide(index) {
+    currentIndex = index;
+    showSlide(currentIndex);
+}
+
+let autoSlide = setInterval(nextSlide, 2000);
+
+const slideContainer = document.querySelector('.slide-container');
+slideContainer.addEventListener('mouseenter', () => clearInterval(autoSlide));
+slideContainer.addEventListener('mouseleave', () => autoSlide = setInterval(nextSlide, 2000));
+
+showSlide(currentIndex);
+
+
+
+
+
+
+// <=========== Image Filter ===========>
 const filterButtons = document.querySelectorAll(".filter-buttons button");
 const filterableCards = document.querySelectorAll(".card-container .card");
 
